7ae12e0ec3002e1b18cda588b58dffbaa2ff4d0a
Andrei McMillan <amcmillan@yieldbot.com>
2015-11-10 17:21:33 -0500

    sh issues

--- publish.sh
+++ publish.sh
@@ -5,31 +5,32 @@
 CAN_PUBLISH=`node can_publish.js`
 EXIT_CODE=$?
 
-echo ">> $EXIT_CODE : $CAN_PUBLISH"
+echo "[$EXIT_CODE] : $CAN_PUBLISH"
 
-if [ $EXIT_CODE == '0' ] ; then
+if [ $EXIT_CODE == '0' ]
+  then
 
-  echo "Creating zip file..."
-  zip -r src.zip src
+    echo "Creating zip file..."
+    zip -r src.zip src
 
-  TOKEN=`node get_refresh_token.js`
+    TOKEN=`node get_refresh_token.js`
 
-  # Uploading a package to update an existing store item
-  curl \
-  -H "Authorization: Bearer $TOKEN"  \
-  -H "x-goog-api-version: 2" \
-  -X PUT \
-  -T src.zip \
-  -s \
-  https://www.googleapis.com/upload/chromewebstore/v1.1/items/$APP_ID
+    # Uploading a package to update an existing store item
+    curl \
+    -H "Authorization: Bearer $TOKEN"  \
+    -H "x-goog-api-version: 2" \
+    -X PUT \
+    -T src.zip \
+    -s \
+    https://www.googleapis.com/upload/chromewebstore/v1.1/items/$APP_ID
 
-  #Publishing an item to the public
-  curl \
-  -H "Authorization: Bearer $TOKEN"  \
-  -H "x-goog-api-version: 2" \
-  -H "Content-Length: 0" \
-  -X POST \
-  -s \
-  https://www.googleapis.com/chromewebstore/v1.1/items/$APP_ID/publish
+    #Publishing an item to the public
+    curl \
+    -H "Authorization: Bearer $TOKEN"  \
+    -H "x-goog-api-version: 2" \
+    -H "Content-Length: 0" \
+    -X POST \
+    -s \
+    https://www.googleapis.com/chromewebstore/v1.1/items/$APP_ID/publish
 
 fi
