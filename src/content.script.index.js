a52027c7d625be9d5e57dab88573865cde8245bd
Andrei McMillan <amcmillan@yieldbot.com>
2016-01-27 16:41:21 -0500

    delay a little before capturing element

--- src/content.script.index.js
+++ src/content.script.index.js
@@ -57,21 +57,12 @@
     if (!target) {
       callback(null);
     } else {
-
-      var offset = target.getBoundingClientRect();
-      //var waitFor = 0;
-      //
-      //if (offset.top < 0) {
-      //  target.scrollIntoView();
-      //  waitFor = 200;
-      //}
-
-      //setTimeout(function() {
-      //offset = target.getBoundingClientRect();
-      chrome.runtime.sendMessage({api: 'screenCapture'}, function(data) {
-        crop(data.img, offset.left, offset.top, offset.width, offset.height, data.zoomFactor, callback);
-      });
-      //}, waitFor);
+      setTimeout(function() {
+        var offset = target.getBoundingClientRect();
+        chrome.runtime.sendMessage({api: 'screenCapture'}, function(data) {
+          crop(data.img, offset.left, offset.top, offset.width, offset.height, data.zoomFactor, callback);
+        });
+      }, 400);
     }
   };
 
